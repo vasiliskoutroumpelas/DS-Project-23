7b548208cbaaf067a4c9a907f51b862cd612efb0
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2023-03-28 22:39:40 +0300

    changed to the version of Filippos

--- src/csvExtractor.cpp
+++ src/csvExtractor.cpp
@@ -2,63 +2,107 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <string>
-#include "csvExtractor.h"
+
 using namespace std;
 
-vector<Record> csvToVector(string filename) {
-    vector<Record> data;
+typedef struct record{
+    string Direction;
+    string Date;
+    string Weekday;
+    string Country;
+    string Commodity;
+    string Transport_Mode;
+    string Measure;
+    long int Value;
+    long int Cumulative;
+    int Year;
     
-    ifstream file(filename);
-    if(!file.good())
-        cout << "error" << endl;
+}Record;
 
-    string line;
-    getline(file, line);
-
-    while (getline(file, line)) {
-        Record record;
-        stringstream ss(line);
-        string cell;
-
-        getline(ss, cell, ',');
-        record.Direction = cell;
-
-        getline(ss, cell, ',');
-        record.Year = stoi(cell);
-
-        getline(ss, cell, ',');
-        record.Date = cell;
-
-        getline(ss, cell, ',');
-        record.Weekday = cell;
-
-        getline(ss, cell, ',');
-        record.Country = cell;
-
-        getline(ss, cell, ',');
-        if (cell[0] == '\"') {
-            string temp = cell.substr(1,cell.size())+',';
-            getline(ss, cell, '"');
-            cell = temp + cell.substr(0, cell.size()-1);
-            getline(ss, cell, ',');
-        }
-        record.Commodity = cell;
-
-        getline(ss, cell, ',');
-        record.Transport_Mode = cell;
-    
-        getline(ss, cell, ',');
-        record.Measure = cell;
-        
-        getline(ss, cell, ',');
-        record.Value = stol(cell);
-
-        getline(ss, cell, ',');
-        record.Cumulative = stol(cell);
-        
-        data.push_back(record);
+void insertData(int counter, string token, vector<Record>& Data){
+    switch (counter)
+    {
+    case 0: Data.back().Direction=token; break;
+    case 1: Data.back().Year=stoi(token); break;
+    case 2: Data.back().Date=token; break;
+    case 3: Data.back().Weekday=token; break;
+    case 4: Data.back().Country=token; break;
+    case 5: Data.back().Commodity=token; break;        
+    case 6: Data.back().Transport_Mode=token; break;
+    case 7: Data.back().Measure=token; break;
+    case 8: Data.back().Value=stol(token); break;
+    case 9: Data.back().Cumulative=stol(token); break;
+    default:
+        break;
     }
-    return data;
 }
 
+
+void splitstring(stringstream& ss, vector<Record>& Data){
+    string token;
+    int counter=0;
+    
+    while(getline(ss, token, ',')){
+        insertData(counter, token, Data);
+        counter++;
+        
+        if(ss.peek()=='"'){
+            ss.get();
+            getline(ss, token, '"');
+            insertData(counter, token, Data);
+
+            //! next comma after ",
+            ss.get();
+            counter++;
+        }
+    }
+    
+}
+
+
+void csvToVector(ifstream& file, vector<Record>& Data){
+string line;
+stringstream ss(line);
+Record* record;
+
+// Remove the first line of the file
+getline(file, line);
+while(getline(file, line)) {
+    
+    record = new Record;
+    Data.push_back(*record);
+    ss<<line;
+    
+    splitstring(ss, Data);
+    
+    //reset stringstream
+    ss.str("");
+    ss.clear();       
+}
+}
+
+int main() {
+    ifstream file("data.csv");
+    if(!file.good()) {
+        cout<<"error"<<endl;
+        return -1;
+    }
+
+    vector<Record> Data;
+    csvToVector(file, Data);
+
+    for (Record row : Data) {
+        cout << "Direction: " << row.Direction << endl;
+        cout << "Year: " << row.Year << endl;
+        cout << "Date: " << row.Date << endl;
+        cout << "Weekday: " << row.Weekday << endl;
+        cout << "Country: " << row.Country << endl;
+        cout << "Commodity: " << row.Commodity << endl;
+        cout << "Transport Mode: " << row.Transport_Mode << endl;
+        cout << "Measure: " << row.Measure << endl;
+        cout << "Value: " << row.Value << endl;
+        cout << "Cumulative: " << row.Cumulative << endl;
+        cout << endl;
+    }        
+    return 0;
+}
