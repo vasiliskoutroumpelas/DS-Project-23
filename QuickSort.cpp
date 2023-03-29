95cdb45fc35f0a6fda685f15114b9cca92dd6d31
Χαράλαμπος Αναστασίου <39501205+VARANE2003@users.noreply.github.com>
2023-03-29 22:05:36 +0300

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -11,39 +11,25 @@ using namespace std;
 //quick sort sorting algorithm
 
 
-typedef struct record{
-    string Direction;
-    int Year;
-    string Date;
-    string Weekday;
-    string Country;
-    string Commodity;
-    string Transport_Mode;
-    string Measure;
-    long int Value;
-    long int Cumulative;
-}Record;
 
-
-
-int Partition(vector <Record> &Data, int s, int e)
+int Partition(vector <Record> &data, int s, int e)
 {
 
-    long int pivot = Data.front().Cumulative;
+    long int pivot = data.front().Cumulative;
     int pIndex = e;
 
     for(int i=e; i>s; i--)
     {
-        if(Data.at(i).Cumulative>pivot)
+        if(data.at(i).Cumulative>pivot)
         {
 
-            swap(Data.at(i), Data.at(pIndex));
+            swap(data.at(i), data.at(pIndex));
             pIndex--;
         }
     }
 
 
-    swap(Data.at(s), Data.at(pIndex));
+    swap(data.at(s), data.at(pIndex));
 
 
     return pIndex;
@@ -51,14 +37,14 @@ int Partition(vector <Record> &Data, int s, int e)
 }
 
 
-void QuickSort(vector <Record> &Data, int s, int e)
+void QuickSort(vector <Record> &data, int s, int e)
 {
 
     if(s<e)
     {
-        int p = Partition(Data, s, e);
-        QuickSort(Data, s, (p-1)); //recursive QuickSort call for left partition
-        QuickSort(Data, (p+1), e); //recursive QuickSort call for right partition
+        int p = Partition(data, s, e);
+        QuickSort(data, s, (p-1)); //recursive QuickSort call for left partition
+        QuickSort(data, (p+1), e); //recursive QuickSort call for right partition
 
     }
 
